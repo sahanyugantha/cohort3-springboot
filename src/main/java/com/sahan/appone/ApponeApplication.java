adad1d5623bb8d635cee3a61e91b33a18a66c048
Sahan Perera <sahanyuganthamail@gmail.com>
2021-06-27 14:37:55 +0530

    cors added

--- src/main/java/com/sahan/appone/ApponeApplication.java
+++ src/main/java/com/sahan/appone/ApponeApplication.java
@@ -2,14 +2,26 @@ package com.sahan.appone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class ApponeApplication {
+@Configuration
+public class ApponeApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApponeApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/**")
+				.allowedOrigins("http://localhost:3000") // for any use "*"
+				.allowedMethods("*"); //"GET", "POST"
+
+	}
 }
